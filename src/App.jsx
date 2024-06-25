245f10651533a3b3f4f7d51b423513229328b2f0
prasanth <prasanth@gmail.com>
2024-06-25 13:53:11 +0530

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+// import './App.css'
+import { Quizapp } from './Quizapp'
+import { JsonDemo } from './JsonDemo'
+import TodoList from './Todo'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+       {/* <Quizapp /> */}
+       {/* <JsonDemo /> */}
+       <TodoList />
+      </div>
+     
+    </>
+  )
+}
+
+export default App
